c6c804e889b774c97b700fe0d135facc72f84e9d
Your Name <you@example.com>
2025-02-23 00:08:03 +0500

    Добавьте файлы проекта.

---
+++ home.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using lab;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string hiddenText;
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        string Hangman = "Hangman";
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write($"Welcome to ");
+
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine(Hangman);
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Thread.Sleep(2000);
+        Console.Clear();
+        Console.Write("Enter your text to start:");hiddenText = Convert.ToString(Console.ReadLine());
+        Console.Clear();
+        Console.WriteLine("");
+        
+
+
+    }
+}
---
+++ lab.cs
@@ -0,0 +1,12 @@
+﻿namespace lab
+{
+    class Game
+    {
+         public static int GetLenght(string arg)
+         {
+            int linesAmount = arg.Length;
+            return linesAmount;
+        
+         }
+    }
+}
