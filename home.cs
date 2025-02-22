033bbf58111ba4d276c7ee4f01c4ed2cafbb93e7
Your Name <you@example.com>
2025-02-23 01:01:29 +0500

    some useless changes

--- home.cs
+++ home.cs
@@ -1,30 +1,8 @@
 ﻿using lab;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] arg)
     {
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        string Hangman = "Hangman";
-
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.Write("Welcome to  ");
-
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine(Hangman);
-
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Thread.Sleep(2000);
-        Console.Clear();
-        Console.Write("To start game you need enter word: ");
-        string hiddenText = Console.ReadLine();
-        Console.Clear();
-        Console.WriteLine("Guess word:");
-        for (int i = 0; i < hiddenText.Length; i++)
-        {
-            Console.Write("_ ");
-        }
-        Console.WriteLine();
-        Game.CheckChars(hiddenText);
+        Game.MainMenu();
     }
 }
--- lab.cs
+++ lab.cs
@@ -5,6 +5,35 @@ namespace lab
 {
     class Game
     {
+        public static void MainMenu()
+        {
+
+        }
+        public static void EngMainMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            string Hangman = "Hangman";
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Welcome to  ");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(Hangman);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.Write("To start game you need enter word: ");
+            string hiddenText = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Guess word:");
+            for (int i = 0; i < hiddenText.Length; i++)
+            {
+                Console.Write("_ ");
+            }
+            Console.WriteLine();
+            Game.CheckChars(hiddenText);
+        }
         public static int GetLength(string arg)
         {
             return arg.Length;
